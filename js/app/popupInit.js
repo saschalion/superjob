6097f53f39e3c9708565880337379b3d7d51f3d7
Alexander Druzhinin <saschalion@gmail.com>
2015-01-19 12:34:35 +0300

    Set id in hidden field, built js

--- js/app/popupInit.js
+++ js/app/popupInit.js
@@ -2,12 +2,15 @@ define(['jquery', 'validate', 'generateContactNumbers', 'validation', 'tabs'],
     function($, validate, generateContactNumbers, validation, tabs) {
         function popupInit() {
             $('.js-popup-link').click(function(e) {
+                var $this = $(this);
+
                 $.ajax({
                     url: '/inc/registration.htm?ver=' + new Date(),
                     success: function(data) {
                         $('.js-popup-inner').html($(data));
                         $('.js-popup').fadeIn();
                         $('.js-page').addClass('_type_with-popup');
+                        $('.js-hidden-field').val($this.data('id'));
 
                         $.each($('.js-form'), function() {
                             validate($(this).closest('.js-registration-box').index() + 1, $(this));
